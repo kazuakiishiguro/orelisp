3000972df1f743aa709399eea74669a287cccc72
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-06-02 23:47:05 +0900

    Add preprocessor for Windows machine

--- prompt.c
+++ prompt.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* for Windows */
+#ifdef _WIN32
+
+#include <string.h>
+
+/* pesudo readline function */
+char *readline(char *prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* pesudo add_history function */
+void add_history(char * unused) {}
+
+#else
 #include <editline/readline.h>
+#endif
 
 static char input[2048];
 
