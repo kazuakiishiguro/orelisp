3f3da13d6a2240285c83dba06802100a16605d52
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-06-03 10:00:32 +0900

    Implement parser with mpc

--- parsing.c
+++ parsing.c
@@ -24,17 +24,44 @@ void add_history(char * unused) {}
 #include <editline/readline.h>
 #endif
 
-static char input[2048];
-
 int main(int argc, char **argv) {
-    puts("Orelisp version 0.0.1");
+    /* Create Some Parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Orelisp    = mpc_new("orelisp");
+
+    /* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                     \
+        number   : /-?[0-9]+/ ;                             \
+        operator : '+' | '-' | '*' | '/' ;                  \
+        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+        orelisp    : /^/ <operator> <expr>+ /$/ ;             \
+        ",
+        Number, Operator, Expr, Orelisp);
+
+    puts("Orelisp version 0.0.2");
     puts("Press Ctl+c to Exit\n");
 
     while (1) {
-        char* input = readline("orelisp> ");
+        char *input = readline("orelisp> ");
         add_history(input);
-        printf("[Echoing....] %s\n", input);
+
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Orelisp, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
     }
+
+    /* undefine and delete our parser */
+    mpc_cleanup(4, Number, Operator, Expr, Orelisp);
+
     return 0;
 }
