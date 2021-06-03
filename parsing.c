540fb31b9245f6b3c7ed5350de7b6f4522ed845a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-06-03 23:28:15 +0900

    Added error handling and lval

--- parsing.c
+++ parsing.c
@@ -24,25 +24,82 @@ void add_history(char * unused) {}
 #include <editline/readline.h>
 #endif
 
-/* evaluate which operator should use */
-long eval_op(long x, char * op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+/* declare lval(lisp value) struct */
+typedef struct {
+    int type;
+    long num;
+    int error;
+} lval;
+
+/* for lval types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* for error types */
+enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
+
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.error = x;
+    return v;
 }
 
-long eval(mpc_ast_t *t) {
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%li", v.num); break;
+        case LVAL_ERR:
+            if (v.error == LERR_DIV_ZERO) {
+                printf("Error: division by zeo.");
+            }
+            if (v.error == LERR_BAD_OP) {
+                printf("Error: Invalid operarotr.");
+            }
+            if (v.error == LERR_BAD_NUM) {
+                printf("Error: Invalid number.");
+            }
+            break;
+    }
+}
+
+void lval_println(lval v) {
+    lval_print(v); putchar('\n');
+}
+
+/* evaluate which operator should use */
+lval eval_op(lval x, char * op, lval y) {
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) {
+        return y.num == 0
+                ? lval_err(LERR_DIV_ZERO)
+                : lval_num(x.num / y.num);
+    }
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t *t) {
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     /* operator is always second child */
     char *op = t->children[1]->contents;
 
     /* store the third child in x */
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     /* iterate remaining children */
     int i = 3;
@@ -80,8 +137,8 @@ int main(int argc, char **argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Orelisp, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
